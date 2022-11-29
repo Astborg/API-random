2984510ee801afdb400a93cd001d9c15e01f655e
Astborg <josefinastborg@hotmail.com>
2022-11-29 11:20:51 +0100

    logo

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/quote.png" width="155" height="155" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
